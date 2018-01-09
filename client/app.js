195054b3b36c3a662662d12218e906b1f064549e
Michael <chiangshtaa@gmail.com>
2018-01-08 17:22:46 -0800

    changed title name

--- client/app.js
+++ client/app.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sample: 'Hello Michael!',
+      sample: 'Cryptocurrency Market',
       clicked: false,
       data: []
     }
